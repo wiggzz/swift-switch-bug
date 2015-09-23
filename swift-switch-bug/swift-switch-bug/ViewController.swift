787d3f8361c52d7b12bed1d1336acb3eb8f8bd31
Will James <wjames@thoughtworks.com>
2015-09-24 08:02:28 +1000

    initial commit

---
+++ swift-switch-bug/swift-switch-bug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  generic-nsobject-bug
+//
+//  Created by Will James on 23/09/2015.
+//  Copyright (c) 2015 Will James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
